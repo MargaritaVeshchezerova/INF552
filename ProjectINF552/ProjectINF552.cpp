477f5cc6c5b05f68fc5a1ea53010bd41fd3f5e12
MargaritaVeshchezerova <rita.2v@gmail.com>
2017-11-08 11:33:53 +0100

    Hello

--- ProjectINF552/ProjectINF552.cpp
+++ ProjectINF552/ProjectINF552.cpp
@@ -15,7 +15,7 @@ int main()
 	IplImage* img = cvLoadImage("C:\\Users\\Margarita\\Downloads\\17974282_1386874394692192_2009764012_n.jpg");
 	cvNamedWindow("Exemple1", CV_WINDOW_AUTOSIZE);
 //	cvShowImage("Exemple1", img);
-	cvWaitKey(0);
+	cvWaitKey(1);
 	cvReleaseImage(&img);
 	cvDestroyWindow("Exemple1");
 	return 0;
