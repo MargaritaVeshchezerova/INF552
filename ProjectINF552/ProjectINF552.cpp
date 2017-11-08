7fd55fb420872a87d8f0fdb57f82f037f26a5fa0
MargaritaVeshchezerova <rita.2v@gmail.com>
2017-11-08 09:55:06 +0100

    Add project files.

---
+++ ProjectINF552/ProjectINF552.cpp
@@ -0,0 +1,22 @@
+// ProjectINF552.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+
+int main()
+{
+	IplImage* img = cvLoadImage("C:\\Users\\Margarita\\Downloads\\17974282_1386874394692192_2009764012_n.jpg");
+	cvNamedWindow("Exemple1", CV_WINDOW_AUTOSIZE);
+//	cvShowImage("Exemple1", img);
+	cvWaitKey(0);
+	cvReleaseImage(&img);
+	cvDestroyWindow("Exemple1");
+	return 0;
+}
